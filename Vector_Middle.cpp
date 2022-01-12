c64396efd16081e34eea99d900b12b30793cb7cd
BorisovetsGleb <glebboriso003@gmail.com>
2022-01-12 23:29:26 +0300

    Добавьте файлы проекта.

---
+++ Vector_Middle.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include "middle_list.h"
+
+using namespace std;
+
+int main()
+{
+    //string a = "12 3 asdfg 23   qwerty 0 ;;", less = "12 ;";
+    //vector <char> out = {'a', 'd', '2', 'd'};
+    setlocale(LC_ALL, "Russian");
+    vector <char> out = {};
+    vector <int> a = {}, b, c, d;
+    //cin >> a;
+    //out_vect_char(itc_strtlist(a));
+    //cout << itc_join(out, a);
+    //out = itc_strtlist(a);
+    //out_vect_char(out);
+    //cout << itc_rmstrspc(a);
+    //cout << itc_rmstrchar(a, less);
+    //cout << itc_sumlst(a);
+    //cout << itc_sum_even_lst(a);
+    //cout << itc_sum_even_part_lst(a);
+    //itc_odd_even_separator_lst(a, b, c);
+
+    /*itc_pos_neg_separator_lst(a, b, c, d);
+    out_vect_int(d);
+    cout << ", ";
+    out_vect_int(c);
+    cout << ", ";
+    out_vect_int(b);*/
+    //itc_odd_even_analysis_lst(a);
+    //itc_pos_neg_analysis_lst(a);
+}
---
+++ pos_neg_separator_lst.cpp
@@ -0,0 +1,27 @@
+#include "middle_list.h"
+
+void itc_pos_neg_separator_lst(const vector <int>& lst, vector <int>& pol, vector <int>& nol, vector <int>& otr)
+{
+	int i = 0, len = lst.size();
+	while (i < len)
+	{
+		if (lst[i] == 0)
+		{
+			nol.push_back(lst[i]);
+		}
+		if (lst[i] > 0)
+		{
+			pol.push_back(lst[i]);
+		}
+		if (lst[i] < 0)
+		{
+			otr.push_back(lst[i]);
+		}
+		i++;
+	}
+	/*out_vect_int(otr);
+	cout << ", ";
+	out_vect_int(nol);
+	cout << ", ";
+	out_vect_int(pol);*/
+}
