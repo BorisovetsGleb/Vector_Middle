f95cac4f3d55504561f6b48366706b5924e8aea5
BorisovetsGleb <92142560+BorisovetsGleb@users.noreply.github.com>
2022-01-20 09:53:21 +0300

    Update strtlist.cpp

--- strtlist.cpp
+++ strtlist.cpp
@@ -3,10 +3,12 @@
 vector <char> itc_strtlist(string str)
 {
 	int len = get_len_str(str), i = 0;
-	if (len < 1) { return {}; }
 	vector <char> out = {};
 	while (i < len) { out.push_back(str[i]); i++; }
-	return out;
+	if (out.size() != 0)
+	{
+		return out;
+	}
 }
 
 int get_len_str(string str)
